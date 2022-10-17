7082452ff54596b079c3ee95a0571340f0bb220c
Soy vitou <soyvitou999@gmail.com>
2022-10-17 11:28:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'fontstyle':'Fuzzy Bubbles'
+      }
+    },
+  },
+  plugins: [],
+}
