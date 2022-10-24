7c07424bbe0b7eb527ded07ae144a3286baff778
Soy vitou <soyvitou999@gmail.com>
2022-10-24 16:53:53 +0700

    new changek

---
+++ schedule.js
@@ -0,0 +1,46 @@
+const buttom = document.getElementById("buttom");
+const items = document.getElementById("items");
+const close = document.getElementById("close");
+const gab = document.getElementById("close");
+const fii = document.getElementById("contact");
+
+buttom.addEventListener("click",() => {
+    if(items.classList.contains("hidden")){
+    items.classList.add("flex");
+    items.classList.remove("hidden");
+    items.classList.add("navsize");
+    buttom.classList.add("layer");
+   
+    buttom.classList.remove("block");
+    buttom.classList.add("hidden");
+    close.classList.add("block");
+    close.classList.remove("hidden");
+    close.classList.add("layer");
+    gab.classList.add("holdsize");
+    
+}
+})
+
+
+
+close.addEventListener("click",() => {
+    if(items.classList.contains("flex")){
+        items.classList.add("hidden");
+        items.classList.remove("flex");
+        items.classList.remove("navsize");
+        buttom.classList.add("layer");
+       
+        buttom.classList.remove("hidden");
+        buttom.classList.add("block");
+        close.classList.add("hidden");
+        close.classList.remove("block");
+        close.classList.add("layer");
+        gab.classList.remove("holdsize");
+        fii.classList.remove("layers");
+        
+    }
+
+    
+
+})
+
