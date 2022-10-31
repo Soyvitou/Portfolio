000839dea61029fb9b8733ba59108950de7c6394
Soy vitou <soyvitou999@gmail.com>
2022-10-31 21:17:35 +0700

    new changeo

--- calculate.js
+++ calculate.js
@@ -1,6 +1,63 @@
 let display = document.getElementById("display");
-let buttons = document.getElementById("button");
-console.log(buttons);
+function changeDispay(number)
+{
+    display.innerHTML += number;
+}
+function clearr()
+{
+    display.innerHTML = null;
+}
+function equal()
+{
+    display.innerHTML = eval(display.innerHTML);
+}
+function deletes()
+{
+    display.innerHTML = display.innerHTML.slice(0,-1);
+}
+// let btn = document.getElementById("buttons");
+// let btndivis = document.getElementById("buttons/");
+// let btnmuiltiple = document.getElementById("buttons*");
+// let btnminusgo = document.getElementById("buttons->");
+// let btn7 = document.getElementById("buttons7");
+// let btn8 = document.getElementById("buttons8")
+// let btn9 = document.getElementById("buttons9")
+// let btnminus = document.getElementById("buttons-")
+// let btn4 = document.getElementById("buttons4")
+// let btn5 = document.getElementById("buttons5")
+// let btn6 = document.getElementById("buttons6")
+// let btnplus = document.getElementById("buttons+")
+// let btn1 = document.getElementById("buttons1")
+// let btn2 = document.getElementById("buttons2")
+// let btn3 = document.getElementById("buttons3")
+// let btnspot = document.getElementById("buttons.")
+// let btnopenparentesis = document.getElementById("buttons(")
+// let btn0 = document.getElementById("buttons0")
+// let btncloseparentesis = document.getElementById("buttons)")
+// btn.addEventListener("click", ()=>{
+//     console.log(btn.innerHTML);
+// })
+// btn1.addEventListener("click", ()=>{
+//     console.log(btn1.innerHTML);
+// })
+// btn2.addEventListener("click", ()=>{
+//     console.log(btn2.innerHTML);
+// })
+// btn3.addEventListener("click", ()=>{
+//     console.log(btn3.innerHTML);
+// })
+// btn4.addEventListener("click", ()=>{
+//     console.log(btn4.innerHTML);
+// })
+// btn5.addEventListener("click", ()=>{
+//     console.log(btn5.innerHTML);
+// })
+// btn6.addEventListener("click", ()=>{
+//     console.log(btn6.innerHTML);
+// })
+// btn7.addEventListener("click", ()=>{
+//     console.log(btn7.innerHTML);
+// })
 
 
 
